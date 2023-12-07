65402b914ec0dbe9fdf847ec4488f3f5c5f6bee3
MotivateBro <itsminingc@gmail.com>
2023-12-07 21:55:59 +0700

    HariGuru

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hari guru</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+        <a class="nav-link active" aria-current="page" href="tokoh.php">Tokoh</a>
+        <a class="nav-link active" aria-current="page" href="tentang.php">Tentang</a>
+        <a class="nav-link active" aria-current="page" href="pesan.php">Buku Tamu</a>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+        </li>
+      </ul>
+      <form class="d-flex">
+      </form>
+    </div>
+  </div>
+</nav>  
+
+<div class="container">
+  <div class="text-center">
+  <img src="HariGuru.jpg" class="rounded" alt="...">
+   <h3>Hari Guru Nasional memiliki makna sebagai bentuk penghormatan dan untuk menunjukkan penghargaan terhadap guru. Tentunya, setiap guru, siswa, sekolah, atau orang-orang sekitar memiliki cara yang berbeda-beda untuk menunjukkan penghargaan tersebut. Namun, bagaimana pun tradisinya yang terpenting adalah memaknai peringatan Hari Guru Nasional sebagai momen untuk menghargai peran Bapak dan Ibu guru dalam mengembangkan pendidikan.</h3>
+  </div>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ tentang.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hari guru</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <a class="nav-link active" aria-current="page" href="index.php">Beranda</a>
+        <a class="nav-link active" aria-current="page" href="tokoh.php">Tokoh</a>
+        <a class="nav-link active" aria-current="page" href="tentang.php">Tentang</a>
+        <a class="nav-link active" aria-current="page" href="pesan.php">Buku Tamu</a>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+        </li>
+      </ul>
+      <form class="d-flex">
+      </form>
+    </div>
+  </div>
+</nav>  
+
+<div class="container">
+  <div class="text-center">
+  <h3>Sejarah hari guru tidak bisa dilepaskan dari keberadaan organisasi PGRI. Melansir situs resmi PGRI, organisasi ini lahir pada tahun 1912.
+
+Awalnya bernama Persatuan Guru Hindia Belanda (PGHB) dengan anggota guru bantu, guru desa, kepala sekolah, sampai pemilik sekolah dari latar pendidikan yang berbeda-beda.
+
+Setelah lahir PGHB, disusul kemudian kelahiran organisasi-organisasi guru lain di berbagai daerah. Pada 1932, 32 organisasi guru sepakat bersatu dengan nama Persatuan Guru Indonesia (PGI).
+
+Pergerakan PGI sempat terhenti pada masa penjajahan Jepang. Pemerintah Negeri Sakura melarang keberadaan organisasi dan menutup sekolah.
+
+Setelah proklamasi kemerdekaan, diadakan Kongres Guru Indonesia pada tanggal 24-25 November 1945 di Surakarta. Di dalam kongres inilah, pada tanggal 25 November 1945, PGRI berdiri dengan tiga tujuan :
+
+1. Mempertahankan dan menyempurnakan Republik Indonesia.
+
+2. Mempertinggi tingkat pendidikan dan pengajaran sesuai dengan dasar-dasar kerakyatan.
+
+3. Membela hak dan nasib buruh umumnya, guru pada khususnya.
+
+Situasi politik yang dinamis tidak menyurutkan semangat PGRI untuk tetap setia dalam pengabdiannya sebagai organisasi perjuangan, organisasi profesi, dan organisasi ketenagakerjaan, yang bersifat unitaristik, dan independen.
+
+Oleh karena itu sebagai bentuk penghormatan kepada guru, pemerintah Republik Indonesia menetapkan 25 November sebagai Hari Guru Nasional. (*)</h3>
+  </div>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
